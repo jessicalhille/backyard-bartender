2ec2baad984f1b71e98fae7086464d7dc38fc084
Jessica Hille <jessicalhille@gmail.com>
2021-11-29 20:53:01 -0600

    fixed bug in api call, got array to load and appear in container

--- assets/script.js
+++ assets/script.js
@@ -22,18 +22,19 @@ var saveSearch = function() {
 }
 
 var getCocktail = function(liquorType) {
-    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + liquorType;
+    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + liquorType + "&appid=1";
 
     // make a request to the url
     fetch(apiUrl)
-    .then(function(response){
+    .then(function(response) {
         if (response.ok) {
-            response.json().then(function(data){
-                displayCocktail(data, liquorType);
-            });
+            return response.json();
         } else {
             return;
-        }
+        };
+    })
+    .then(function(data) {
+        displayCocktail(data, liquorType);
     });
 }
 
@@ -41,18 +42,31 @@ var displayCocktail = function(drinks, searchLiquor) {
     resultsContainerEl.textContent = "";
     searchTermEl.textContent = searchLiquor;
 
-    var cocktailName = document.createElement("span");
-    cocktailName.textContent = "Cocktail Name: ";
-    cocktailName.classList = "recipe";
+    for (var i = 0; i < drinks.drinks.length; i++) {
+        var cocktailUrl = drinks.drinks[i].strDrinkThumb;
+        var cocktailNameArr = drinks.drinks[i].strDrink;
+        var cocktailIdArr = drinks.drinks[i].idDrink;
 
-    var cocktailId = document.createElement("span");
-    cocktailId.textContent = "Cocktail ID: ";
-    cocktailId.classList = "recipe";
+        var apiIdUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + cocktailIdArr + "&appid=1";
 
-    resultsContainerEl.appendChild(cocktailName);
-    resultsContainerEl.appendChild(cocktailId);
+        var cocktailImg = cocktailUrl + "/preview";
+
+        var cocktailId = document.createElement("button");
+        cocktailId.classList = "recipe";
+        cocktailId.addEventListener("click", function(event) {
+            apiIdUrl;
+        });
+
+        var cocktailRecipe = document.createElement("ul");
+        cocktailRecipe.textContent = cocktailNameArr;
+        cocktailRecipe.classList = "list-item flex-row justify-space-between";
+
+        cocktailId.appendChild(cocktailRecipe);
+
+        resultsContainerEl.appendChild(cocktailId);
+    };
 }
 
-userFormEl.addEventListener("submit", formSubmit);
 
-getCocktail();
+
+userFormEl.addEventListener("submit", formSubmit);
